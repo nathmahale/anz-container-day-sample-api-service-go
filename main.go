6bbcaa08419fbf104aea4f767667e195a30fffe7
Sainath Mahale <dmfpython@gmail.com>
2020-01-31 13:36:37 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Not Hello World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
